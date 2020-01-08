ca43091cd7be3d71c3cc8245a1435d64e1df8884
WhyAre <laiyr52@gmail.com>
2020-01-08 04:01:35 -0800

    Removed unused imports

--- web-browsing-bot/src/main/java/com/webbrowsingbot/app/PageAction.java
+++ web-browsing-bot/src/main/java/com/webbrowsingbot/app/PageAction.java
@@ -4,8 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.io.FileReader;
 import java.lang.reflect.Type;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import org.openqa.selenium.By;
--- web-browsing-bot/src/main/java/com/webbrowsingbot/app/Utils.java
+++ web-browsing-bot/src/main/java/com/webbrowsingbot/app/Utils.java
@@ -1,8 +1,6 @@
 package com.webbrowsingbot.app;
 
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
