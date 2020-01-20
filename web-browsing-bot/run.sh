88376053c601bef018acb5d9cafb8e522b507da8
WhyAre <laiyr52@gmail.com>
2020-01-20 11:55:09 +0800

    Recompiled jar files to the latest source code

--- web-browsing-bot/run.sh
+++ web-browsing-bot/run.sh
@@ -1,3 +1,3 @@
 #java -jar target/build/WebBrowsingBot.jar --time 600 --action google_action.json "https://www.google.com/" --headless
 #docker run -itv $(pwd):/utils --rm --name browsingbot browsing-bot --action /utils/google_action.json "https://www.google.com/"
-java -jar target/build/WebBrowsingBot.jar --time 600 --login json_files/outlook_login.json "outlook.live.com"
+java -jar target/build/WebBrowsingBot.jar --time 600 --login "$(cat json_files/outlook_login.json)" --action "$(cat json_files/outlook_action.json)" "outlook.live.com"
