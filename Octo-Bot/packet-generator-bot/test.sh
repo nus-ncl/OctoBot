d2f161cff87ada92806835608fdb05a3f96fea79
krusagiz <acgmelvin@gmail.com>
2020-05-29 17:33:40 +0800

    Added documentation to to the test shell script

--- Octo-Bot/packet-generator-bot/test.sh
+++ Octo-Bot/packet-generator-bot/test.sh
@@ -1,13 +1,19 @@
+# Runs all unit tests in the ./tests directory
+
+# Setup permissions
 chmod a+x *.sh &&
 
+# Copy required files to the right folder
 cp -r captures bot && 
 cp -r tests/test_*.py bot 
 
+# Run all tests
 for test in bot/test_*.py
 do
     python $test
 done
 
+# Remove copied files
 rm -rf bot/captures &&
 rm -f bot/test_*.py
 
