24599c98641a0e26db7cde38232ccbd2903456dd
Joel Chang <42378287+joelczk@users.noreply.github.com>
2020-06-03 12:18:57 +0000

    Add more ui tests

--- Octo-Bot/healthcare-web-bot/uitest.py
+++ Octo-Bot/healthcare-web-bot/uitest.py
@@ -1,13 +1,68 @@
+
 import unittest
+import warnings
+import io
+import sys
 from util.Driver.parser import *
-from main import getDriver
+from test import getDriver
+
+
+def test():
+    username = "S1234567B"
+    password = "easyP@ssw0rd"
+    url = "http://10.10.0.112"
+    out = io.StringIO()
+    sys.stdout = out
+    driver = getDriver(username, password, url)
+    sys.stdout = sys.__stdout__
+    print(out.getvalue())
 
 class Testing(unittest.TestCase):
+
+    test_failed = False
+
+    def setUp(self):
+        warnings.simplefilter('ignore',category = DeprecationWarning)
+
+    def teardown(self):
+        super(Testing, self).teardown()
+        print(self.test_failed)
+
+    # def test_string(self):
+    #     a = 'some'
+    #     b = 'some'
+    #     self.assertEqual(a, b)
+
+    # def test_boolean(self):
+    #     a = True
+    #     b = True
+    #     self.assertEqual(a, b)
     
     def testUrl(self):
         url = str(getUrl())
         actualUrl = "http://10.10.0.112"
         self.assertEqual(url, actualUrl)
-        
+    
+    def testDriver(self):
+        username = "S1234567B"
+        password = "easyP@ssw0rd"
+        url = "http://10.10.0.112"
+        driver = getDriver(username, password, url)
+        self.assertIsNotNone(driver)
+    
+    def testLogin(self):
+        isLoggedIn = False
+        username = "S1234567B"
+        password = "easyP@ssw0rd"
+        url = "http://10.10.0.112"
+        out = io.StringIO()
+        sys.stdout = out
+        driver = getDriver(username, password, url)
+        sys.stdout = sys.__stdout__
+        if "Logged in" in out.getvalue():
+            isLoggedIn = True
+        self.assertTrue(isLoggedIn)
+
 if __name__ == '__main__':
     unittest.main()
+    # test()
