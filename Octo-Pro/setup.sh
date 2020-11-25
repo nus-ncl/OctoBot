5b31ad71c049ff181e9f20f1b7b41462b4c0438f
Aris Cahyadi Risdianto <aris.risdianto@gmail.com>
2020-11-25 16:45:12 +0800

    [Octo-Pro] Adding Persistent Volume Sharing (#30)
    Adding persistent volume sharing using NFS to support bot movement/migration. This PR contains the following commits:
    * Create a new playbook for volume support
    * Adding menu for volume configuration.
    * Fixing NFS mount server address and directory
    * Fixing Ansible variable for NFS mount.

--- Octo-Pro/setup.sh
+++ Octo-Pro/setup.sh
@@ -4,12 +4,13 @@ sudo apt-get update
 sudo apt-get install ansible -y
 
 while true; do
-	read -p "Please select your installation target? [Controller (c), Worker(w), All (a) or Exit (e)] " cwae
+	read -p "Please select your installation target? [Controller (c), Worker(w), All (a), Volume(v), or Exit (e)] " cwae
     case $cwae in
         [Cc]* ) echo "Installing Controller Node"; ansible-playbook -i hosts master-playbook.yml; exit;;
         [Ww]* ) echo "Installing Worker Nodes"; ansible-playbook -i hosts node-playbook.yml; exit;;
-	    [Aa]* ) echo "Installing Controller and Worker Nodes"; ansible-playbook -i hosts master-playbook.yml; ansible-playbook -i hosts node-playbook.yml; exit;;
-	    [Ee]* ) exit;;
-	    * ) echo "Please select (c), (w) or (a)";;
+        [Aa]* ) echo "Installing Controller and Worker Nodes"; ansible-playbook -i hosts master-playbook.yml; ansible-playbook -i hosts node-playbook.yml; exit;;
+        [Vv]* ) echo "Installing NFS and Configuring Shared Volumes"; ansible-playbook -i hosts volume-support-playbook.yaml; exit;;
+        [Ee]* ) exit;;
+        * ) echo "Please select (c), (w), (a) or (v) ?";;
     esac
 done
