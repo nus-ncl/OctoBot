4d44293440beb12a98797a7856c484416540a20a
akhilvuputuri <akhilvuputuri@gmail.com>
2021-11-16 00:42:12 -0800

    Take all parameters as command line input

--- Octo-Bot/banking-web-application-bot/main.py
+++ Octo-Bot/banking-web-application-bot/main.py
@@ -34,20 +34,30 @@ def getDriver(showDisplay):
     # driver.get(url)
     return driver
 
-username = "test-1"
-oldPassword = "Oldpassword@1"
-name = "Test 1"
-email = "test1@test.com"
-newPassword = "NewPassword@1"
-
 
 
 if __name__== "__main__":
     parser = argparse.ArgumentParser(description = \
         "Arguments for program")
 
-    parser.add_argument('-url', type=str, \
-                    help='Target Website URL', default = 'http://127.0.0.1:8080/')
+    parser.add_argument('-url', metavar = 'url of application', type=str, \
+        help='Target Website URL', default = 'http://127.0.0.1:8080/')
+
+    parser.add_argument('-user', metavar = 'username', type=str, \
+        help='Username to be used', default = 'test-1"')
+
+    parser.add_argument('-orgPass', metavar = 'password', type=str, \
+        help='Password to be used', default = 'Oldpassword@1')
+
+    parser.add_argument('-newPass', metavar = 'new password', type=str, \
+        help='New password to be changed to', default = 'NewPassword@1')
+
+    parser.add_argument('-name', metavar = 'name of user', type=str, \
+        help='Name to be used', default = 'Test 1')
+
+    parser.add_argument('-email', metavar = 'email of user', type=str, \
+        help='Email to be used', default = 'test1@test.com')
+
     parser.add_argument('-d', metavar = 'display', type=int, \
         help='Time to sleep between crawling of website links', default = 0)
     
@@ -55,7 +65,11 @@ if __name__== "__main__":
 
     showDisplay = args.d
     url = args.url
-    print(showDisplay)
+    username = args.user
+    oldPassword = args.orgPass
+    name = args.name
+    email = args.email
+    newPassword = args.newPass
 
     if (showDisplay != 1):
 
