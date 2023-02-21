a62e9ba225f931880e2fbe4535ad38c3defea0e9
Aris C. Risdianto <aris.risdianto@gmail.com>
2023-02-21 15:16:59 +0800

    Fixing the execution of the web driver.

--- Octo-Bot/javascript-web-bot/build.sh
+++ Octo-Bot/javascript-web-bot/build.sh
@@ -1,11 +1,11 @@
 #Set permissions
 chmod a+x *.sh &&
 
-if [[ ! -d "web-browsing-bot/target/build/" ]]
+if [[ ! -d "web-browsing-bot/target/classes/" ]]
 then
     cd web-browsing-bot
     mvn clean install
     cd ..
 fi
 
-sudo docker build . -t browsing-bot
+sudo docker build . -t javascript-web-bot
